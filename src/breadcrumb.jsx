2846757da36f0db94e5d142b63e50ce97b0d6880
es-kartikeygupta <kartikey.gupta@evren.co>
2024-09-10 20:33:13 +0530

    changed route

--- src/breadcrumb.jsx
+++ src/breadcrumb.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Breadcrumb = () => {
+  const location=useLocation();
+
     return (
         <nav className="flex flex-wrap  font-poppins text-base text-[#606060]" aria-label="Breadcrumb">
             <ol className="list-none p-0 inline-flex">
@@ -13,7 +16,7 @@ const Breadcrumb = () => {
 
                 </li>
                 <li className="flex items-center">
-                    <a href="/category" className="text-gray-600 hover:text-gray-900">Cookware</a>
+                    <a href="/category" className="text-gray-600 hover:text-gray-900">{location.pathname==="/cookware"?"Cookware":location.pathname==="/drinkware"?"Drinkware":"Serveware"}</a>
                     <svg className="w-3 h-3 mx-2 text-[#606060]" width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                     {/* <line x1="35" y1="85" x2="65" y2="15" stroke="#606060" stroke-width="8" /> */}
                     </svg>
